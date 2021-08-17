b8d8d7095e2e83f33f7cba9eaa3fd87951461c87
tim-krebs <timkrebs9@gmail.com>
2021-08-17 15:29:35 +0200

    modified package parameter

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gaussian_distribution',
+      version='1.0',
+      description='Gaussian and Binomnal distribution',
+      packages=['gaussian_distribution'],
+      author='Tim Krebs',
+      author_email='timkrebs9@gmail.com',
+      zip_safe=False)
